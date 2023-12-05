1d0a79bf02c5d0148f93dd883415037d59d1d9bc
abhiavi7 <118717274+abhiavi7@users.noreply.github.com>
2023-12-05 20:19:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const byteSize = (str) => {
-  // write your code here
+  // write your code herefunction byteSize(str) {
+  // Create a Blob object with the given string
+  const blob = new Blob([str]);
+
+  // Get the size of the Blob in bytes
+  const sizeInBytes = blob.size;
+
+  return sizeInBytes;
+}
+const stringToMeasure = "Hello, 你好, привет!";
+const sizeInBytes = byteSize(stringToMeasure);
+
+console.log(`Size of the string in bytes: ${sizeInBytes}`);
 };
 
 // Do not change the code below
